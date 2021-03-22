9196c60f34b2b304d0b1e573f3ee72aa7327e4d6
bppwd2021-Anderson <58078083+bppwd2021-Anderson@users.noreply.github.com>
2021-03-21 20:34:06 -0400

    Testing version differences

--- Moo Moo to the Moon/Assets/Shooting.cs
+++ Moo Moo to the Moon/Assets/Shooting.cs
@@ -12,7 +12,7 @@ public class Shooting : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(-speed, 0);
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
 
     }
